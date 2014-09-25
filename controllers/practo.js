2486eb77ba210f41b2f7b52c4f3cc5a6774018a1
I076326 <subhobrata.dey@sap.com>
2014-09-25 16:15:30 +0530

    new commit

--- controllers/practo.js
+++ controllers/practo.js
@@ -3,10 +3,14 @@ exports.getPracto = function(req,res){
     var url_parts = url.parse(req.url,true);
     
     var query = url_parts.query;
-    var location = query.location;
+    var paramName = query.paramName;
+    var paramVal = query.paramVal;
+    
+    console.log(paramName);
+    console.log(paramVal);
     var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET','https://api.practo.com/doctors?city=' + location + '&with_relations=true',false);
+    xmlhttp.open('GET','https://api.practo.com/doctors?' + paramName + '=' + paramVal + '&with_relations=true',false);
     xmlhttp.setRequestHeader("X-API-KEY","qPyOrdb7NEsXfvJ4CHyDisLa+ds=");
     xmlhttp.setRequestHeader("X-CLIENT-ID","27979c0c-52ac-4e33-8a23-710042499d7f");
     xmlhttp.send();
