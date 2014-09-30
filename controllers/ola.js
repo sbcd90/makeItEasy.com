b072e6121e55fb335e52624af7f59ef48136b333
sbcd90 <sbcd90@gmail.com>
2014-10-01 01:11:30 +0530

    adding taxi fare finder api uses

--- controllers/ola.js
+++ controllers/ola.js
@@ -5,13 +5,12 @@ exports.getAvailability = function(req,res){
     var query = url_parts.query;
     var center_lat = query.centerlat;
     var center_long = query.centerlong;
+    var chamber_lat = query.chamberlat;
+    var chamber_long = query.chamberlong;
     
     var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET','http://console.olacabs.com/city/3/category/1/availability.json?lat=' + center_lat + '&lng=' + center_long,false);
-    xmlhttp.setRequestHeader("X-User-Agent","OlaDocumentation.v1");
-    xmlhttp.setRequestHeader("version","v1");
-    xmlhttp.setRequestHeader("X-Ola-API-Key-Auth","e033484d521146306308838b511c6877");
+    xmlhttp.open('GET','https://api.taxifarefinder.com/fare?key=wEGuch2qepAq&entity_handle=Bangalore-India&origin=' + center_lat + ',' + center_long + '&destination=' + chamber_lat + ',' + chamber_long,false);
     xmlhttp.send();
 //    console.log(JSON.parse(xmlhttp.responseText));
     res.end(xmlhttp.responseText);
